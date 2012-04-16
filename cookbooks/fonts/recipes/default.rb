13cc2f8ad117e80db38a3a0f890bee395741c810
Marcin Kulik <marcin.kulik@gmail.com>
2012-04-16 02:13:14 +0200

    Droid and Bitstream Vera fonts for Ubuntu

--- cookbooks/fonts/recipes/default.rb
+++ cookbooks/fonts/recipes/default.rb
@@ -5,4 +5,7 @@ when 'fedora'
   package "google-droid-sans-fonts"
   package "google-droid-sans-mono-fonts"
   package "google-droid-serif-fonts"
+when 'ubuntu'
+  package 'ttf-droid'
+  package 'ttf-bitstream-vera'
 end
